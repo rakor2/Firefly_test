86ede3c0bd1a7ff724e35286f2ed40587261b46a
rakor2 <winster2207@gmail.com>
2025-03-14 12:16:17 +0300

    xyz slider changes

--- Mods/ScriptExtender/Lua/Client/UI/MainTab.lua
+++ Mods/ScriptExtender/Lua/Client/UI/MainTab.lua
@@ -3,8 +3,9 @@ function MainTab(mt)
     if MainTab ~= nil then return end
     MainTab = mt
 
-    local xyzSlider = mt:AddSlider("", 0, -5, 5, 0.001)
+    local xyzSlider = mt:AddSlider("X,Y,Z", 0, -5, 5, 0.001)
     xyzSlider.Components = 3
+    xyzSlider.Value = {0,2,1,0}
     xyzSlider.OnChange = function(value)
         xyzSliderValue(value)
     end
